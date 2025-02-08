a93dc232e76ca5a9731f5ac4dd52e46cd3b7d14d
RuslanGon <ruslan441006@gmail.com>
2025-02-08 10:50:54 +0200

    wer

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      Пример компонента с использованием Tailwind CSS.
+    </div>
+  );
+};
+
+export default App;
