ef7e17f5c5e4b69e3afcb1c6baa0314c67a4a18b
RuslanGon <ruslan441006@gmail.com>
2025-02-10 15:14:07 +0200

    wer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-// import {Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero} from './components'
-import Billing from "./components/Billing.jsx"
-import Business from "./components/Business.jsx"
-import CTA from "./components/CTA.jsx"
-import CardDeal from "./components/CardDeal.jsx"
-import Clients from "./components/Clients.jsx"
-import Footer from "./components/Footer.jsx"
-import Hero from "./components/Hero.jsx"
-import Navbar from "./components/Navbar.jsx"
-import Stats from "./components/Stats.jsx"
-import Testimonials from "./components/Testimonials.jsx"
 import styles from "./style"
+import {Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero} from './components'
+// import Billing from "./components/Billing.jsx"
+// import Business from "./components/Business.jsx"
+// import CTA from "./components/CTA.jsx"
+// import CardDeal from "./components/CardDeal.jsx"
+// import Clients from "./components/Clients.jsx"
+// import Footer from "./components/Footer.jsx"
+// import Hero from "./components/Hero.jsx"
+// import Navbar from "./components/Navbar.jsx"
+// import Stats from "./components/Stats.jsx"
+// import Testimonials from "./components/Testimonials.jsx"
+// import styles from "./style"
 
 const App = () => {
   return (
